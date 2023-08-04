24d8dfd65d6aa5e453d0110678b10996a0642f42
Caitano <josias.m.caitano@accenture.com>
2023-08-04 08:46:33 -0300

    wip: creat forms for art

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { IonicModule } from '@ionic/angular';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +16,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
